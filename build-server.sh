8c71a43de2fbe3804f46467196cef4c29b5b691a
josh <josh@abc>
2026-01-04 18:59:11 +0800

    .

--- build-server.sh
+++ build-server.sh
@@ -1,6 +1,6 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
     go build -ldflags="-s -w" -o /server ./cmd/host-proxy/main.go
-copy ./cmd/host-proxy/config.yaml /config.yaml
+copy ./cmd/host-proxy/config.yaml ./config.yaml
 
 chmod +x /server
 
